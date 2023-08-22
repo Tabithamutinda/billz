ac913fe0bbd6c9b00653ccb585bc398872d070b1
Tabithamutinda <tabbithamutinda@gmail.com>
2023-08-22 22:05:52 +0300

    Updates

--- app/src/main/java/com/example/billz/ui/auth/LoginPage.kt
+++ app/src/main/java/com/example/billz/ui/auth/LoginPage.kt
@@ -3,15 +3,18 @@ package com.example.billz.ui.auth
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.opengl.Visibility
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
 import com.example.Model.Constants
 import com.example.Model.LoginRequest
+import com.example.Model.LoginResponse
 import com.example.ViewModel.UserViewModel
 import com.example.billz.databinding.ActivityLoginBinding
 import com.example.billz.home.MainActivity
@@ -21,6 +24,7 @@ class LoginPage:  AppCompatActivity(){
     lateinit var binding: ActivityLoginBinding
     lateinit var sharedPrefs: SharedPreferences
     val userViewModel: UserViewModel by viewModels()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,17 +48,29 @@ class LoginPage:  AppCompatActivity(){
                     email = email, password = password
                 )
             userViewModel.loginUser(logRequest)
-
-            val intent = Intent(baseContext, MainActivity::class.java)
-            startActivity(intent)
+            redirectUser()
         }
         binding.registerText.setOnClickListener {
             val intent = Intent(baseContext, RegistrationPage::class.java)
             startActivity(intent)
         }
 
+    }
+    fun redirectUser() {
+        //to check whether the user is logged in or not
+        userViewModel.loginResponseLiveData.observe(this) { loginResponse ->
 
+            if (!loginResponse.access_token.isNullOrEmpty()) {
+                binding.progressBar3.visibility = View.VISIBLE
+                Toast.makeText(baseContext, "Login successful", Toast.LENGTH_LONG).show()
+                startActivity(Intent(baseContext,MainActivity::class.java))
+            }
+        }
+        userViewModel.loginErrorLiveData.observe(this) { error ->
+            Toast.makeText(baseContext, error, Toast.LENGTH_SHORT).show()
+            startActivity(Intent(baseContext,LoginResponse::class.java))
 
+        }
     }
 
 }
