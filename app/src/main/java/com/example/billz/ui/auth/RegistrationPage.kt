40919082adc085abd932d3d9b07612f788a22745
Tabithamutinda <tabbithamutinda@gmail.com>
2023-08-23 14:54:49 +0300

    Updated registration page

--- app/src/main/java/com/example/billz/ui/auth/RegistrationPage.kt
+++ app/src/main/java/com/example/billz/ui/auth/RegistrationPage.kt
@@ -11,6 +11,7 @@ import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.Model.Constants
+import com.example.Model.LoginResponse
 import com.example.Model.RegistrationRequest
 import com.example.ViewModel.UserViewModel
 import com.example.billz.R
@@ -18,7 +19,7 @@ import com.example.billz.databinding.ActivityLoginBinding
 import com.example.billz.databinding.ActivityRegistrationBinding
 import com.example.billz.home.MainActivity
 
-class RegistrationPage : AppCompatActivity(){
+class RegistrationPage : AppCompatActivity() {
     private val handler = Handler(Looper.getMainLooper())
     lateinit var sharedPrefs: SharedPreferences
     lateinit var binding: ActivityRegistrationBinding
@@ -30,61 +31,62 @@ class RegistrationPage : AppCompatActivity(){
         sharedPrefs = getSharedPreferences(Constants.PREFS_FILE, Context.MODE_PRIVATE)
         setContentView(binding.root)
 
-        binding.getStartedRegisterButton.setOnClickListener{
+        binding.getStartedRegisterButton.setOnClickListener {
+            binding.progressBar3.visibility = View.VISIBLE
             val firstName = binding.firstNameInput.text.toString()
-            if(firstName.isEmpty()){
+            if (firstName.isEmpty()) {
                 binding.firstNameInput.setError("Enter first name")
             }
             val lastName = binding.lastNameInput.text.toString()
-            if(lastName.isEmpty()){
+            if (lastName.isEmpty()) {
                 binding.lastNameInput.setError("Enter last name")
             }
             val email = binding.emailInput.text.toString()
-            if(email.isEmpty()){
+            if (email.isEmpty()) {
                 binding.emailInput.setError("Enter email")
             }
             val phone = binding.phoneInput.text.toString()
-            if(phone.isEmpty()){
+            if (phone.isEmpty()) {
                 binding.phoneInput.setError("Enter phone number")
             }
             val password = binding.passwordInput.text.toString()
-            if(password.isEmpty()){
+            if (password.isEmpty()) {
                 binding.passwordInput.setError("Enter password")
             }
 
-            val registerUser = RegistrationRequest(first_name = firstName, last_name = lastName, email = email, phone_number = phone, password = password)
+            val registerUser = RegistrationRequest(
+                first_name = firstName,
+                last_name = lastName,
+                email = email,
+                phone_number = phone,
+                password = password
+            )
             userViewModel.registerUser(registerUser)
 
             redirectUser()
         }
 
-        binding.before.setOnClickListener{
+        binding.before.setOnClickListener {
             val intent = Intent(baseContext, LoginPage::class.java)
             startActivity(intent)
         }
     }
 
     fun redirectUser() {
-        //to check whether the user is logged in or not
-        var token = sharedPrefs.getString(Constants.ACCESS_TOKEN, Constants.EMPTY_STRING)
-        if (token!!.isNotEmpty()){
-            startActivity(Intent(baseContext,MainActivity::class.java))
-        }
-        else{
-            startActivity(Intent(baseContext,LoginPage::class.java))
-        }
-    }
+        //to check whether the user is registered or not
 
-    override fun onResume() {
-        super.onResume()
         userViewModel.registrationResponseLiveData.observe(this) { regResponse ->
 
             if (!regResponse.user.user_id.isNullOrEmpty()) {
+                binding.progressBar3.visibility = View.VISIBLE
                 Toast.makeText(baseContext, "Registration successful", Toast.LENGTH_LONG).show()
+                startActivity(Intent(baseContext,MainActivity::class.java))
             }
         }
         userViewModel.regErrorLiveData.observe(this) { error ->
+            binding.progressBar3.visibility = View.VISIBLE
             Toast.makeText(baseContext, error, Toast.LENGTH_SHORT).show()
+            startActivity(Intent(baseContext, RegistrationPage::class.java))
         }
     }
 }
