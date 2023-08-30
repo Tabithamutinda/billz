778a91f858176039dcdc00e2689228c234ecca3b
Tabithamutinda <tabbithamutinda@gmail.com>
2023-08-30 18:22:45 +0300

    Add (Add bills icon)

--- app/src/main/java/com/example/billz/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/billz/ui/home/MainActivity.kt
@@ -3,21 +3,17 @@ package com.example.billz.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import com.example.billz.AddBills
 import com.example.billz.PaidBills
 import com.example.billz.R
 import com.example.billz.Settings
 import com.example.billz.Summary
 import com.example.billz.UpcomingBills
-import com.example.billz.databinding.ActivityLoginBinding
 import com.example.billz.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -25,8 +21,9 @@ class MainActivity : AppCompatActivity() {
 
         val firstFragment = Summary()
         val secondFragment = UpcomingBills()
-        val thirdFragment = PaidBills()
-        val fourthFragment = Settings()
+        val thirdFragment = AddBills()
+        val fourthFragment = PaidBills()
+        val fifthFragment = Settings()
 
         setCurrentFragment(firstFragment)
 
@@ -35,9 +32,9 @@ class MainActivity : AppCompatActivity() {
             when (it.itemId) {
                 R.id.nav_summary -> setCurrentFragment(firstFragment)
                 R.id.nav_upcoming -> setCurrentFragment(secondFragment)
-                R.id.nav_paid -> setCurrentFragment(thirdFragment)
-                R.id.nav_settings -> setCurrentFragment(thirdFragment)
-
+                R.id.nav_add -> setCurrentFragment(thirdFragment)
+                R.id.nav_paid -> setCurrentFragment(fourthFragment)
+                R.id.nav_settings -> setCurrentFragment(fifthFragment)
             }
             true
         }
